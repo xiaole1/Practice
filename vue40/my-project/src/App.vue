e7c22931428fa3b689a3467991a99170c6e00fe8
My Namee <403628444@qq.com>
2018-08-08 22:03:05 +0800

    change1

--- vue40/my-project/src/App.vue
+++ vue40/my-project/src/App.vue
@@ -2,6 +2,8 @@
   <div id="app">
     <img src="./assets/logo.png">
     <my-component />
+    <p>Prop验证</p>
+    <example :msg_null="123+456" msg_string="this is string" />
     <router-view/>
   </div>
 </template>
